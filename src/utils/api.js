52da7a81b5f22a0c2d8ba7b2e2bfed3db19dc094
ToniaStack <toniaebereonyeka@gmail.com>
2026-09-03 21:59:57 +0100

    add render link

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 // src/utils/api.js
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "https://job-tracker-4khl.onrender.com"; // 
 
 const API_URL = `${API_BASE_URL}/api/applications`;
 const JOBS_URL = `${API_BASE_URL}/api/jobs`;
